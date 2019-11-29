96537b22d1b45f74812f647643597e4aee13e81b
Juan Domandl <jdomandl@Juans-MacBook-Pro.local>
2019-11-29 12:58:08 -0300

    Updates

---
+++ precache-manifest.c2bf759316c06299c79fb3cc34b0d6c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a09ae12c47fcac9f711c1beeda86513",
+    "url": "/vidrieraS/index.html"
+  },
+  {
+    "revision": "7cd7f499c7860395a7c7",
+    "url": "/vidrieraS/static/css/2.7475c4dc.chunk.css"
+  },
+  {
+    "revision": "21f9c23db73ccbe88d3c",
+    "url": "/vidrieraS/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "7cd7f499c7860395a7c7",
+    "url": "/vidrieraS/static/js/2.c2229b54.chunk.js"
+  },
+  {
+    "revision": "21f9c23db73ccbe88d3c",
+    "url": "/vidrieraS/static/js/main.2c7f3d23.chunk.js"
+  },
+  {
+    "revision": "6d9840488867d85ee683",
+    "url": "/vidrieraS/static/js/runtime~main.5cf78081.js"
+  },
+  {
+    "revision": "664a13476fec06e901b20b0b18389a69",
+    "url": "/vidrieraS/static/media/construction.664a1347.svg"
+  },
+  {
+    "revision": "72c6f4a261f510e4495e137d59f7b51a",
+    "url": "/vidrieraS/static/media/logoJusticia.72c6f4a2.png"
+  }
+]);
